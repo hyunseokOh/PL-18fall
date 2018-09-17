6bd886206b76bb60c51f375f1d683eb2d6cce5f0
Hyunseok <hyunseok0222@gmail.com>
2018-09-18 02:37:01 +0900

    Error fix

--- hw2/ex4.ml
+++ hw2/ex4.ml
@@ -11,22 +11,22 @@ module type Queue =
 module IntListQ = 
 	struct
 		type element = int list
-		type queue = ((int list) * (int list))
+		type queue = ((int list list) * (int list list))
 		exception EMPTY_Q
 		let emptyQ = ([],[])
 		let enQ(q,x) = match q with
-			| (l1,l2) -> (x::l1,l2)
-		
-		let deQ q =  
-			let rec clean l1 l2 = match (l1,l2) with
+            | (l1,l2) -> (x::l1,l2)
+			
+        let deQ q =
+            let rec clean l1 l2 = match (l1,l2) with
 				| ([],[]) -> raise EMPTY_Q
 				| (hd::tl,_) -> clean tl (hd::l2)
-				| ([],hd::tl) -> (hd,([],tl))
+                | ([],hd::tl) -> (hd,([],tl))
 			in
 
 			match q with
 			| (l,[]) -> clean l []
-			| (l,x::ll) -> (x,(l,ll))
+            | (l,x::ll) -> (x,(l,ll))
 	end
 
 module ValidIntListQ = (IntListQ : Queue)
