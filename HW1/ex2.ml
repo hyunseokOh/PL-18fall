ebc14176b4613527bfbc56d6499adedafa8e83fd
Hyunseok <hyunseok0222@gmail.com>
2018-09-18 01:33:36 +0900

    "Folder Change"

--- HW1/ex2.ml
+++
@@ -1,8 +0,0 @@
-let rec sumprod : (int * int -> float) * int * int -> float = fun(mm,n,k) ->
-	let rec prod : (int * int -> float) * int * int -> float = fun(mm,n,k) ->
-		if k<1 then 1.0
-		else prod(mm,n,k-1) *. mm(n,k)
-	in
-
-	if n<1 then 0.0
-	else sumprod(mm,n-1,k) +. prod(mm,n,k)
