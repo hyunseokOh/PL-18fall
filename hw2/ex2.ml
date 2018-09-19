32a6cf414f825dc8d1e297e5413f2dbaf980860a
Hyunseok <hyunseok0222@gmail.com>
2018-09-20 02:32:53 +0900

    Times error fix

--- hw2/ex2.ml
+++ hw2/ex2.ml
@@ -6,11 +6,39 @@ type ae = CONST of int
 
 exception InvalidArgument
 
-let rec diff: ae * string -> ae = fun(exp,var) ->
-	match exp with
+let rec diff: ae * string -> ae = fun(exp,var) -> match exp with
 	| CONST _ -> CONST 0
-	| VAR s -> if s==var then CONST 1
+	| VAR s -> if s=var then CONST 1
 		       else CONST 0
-	| POWER (s,n) -> if s==var then
-						(if n==0 then CONST 0
-    | TIMES l1
+	| POWER (s,n) -> if s=var then
+                        (if n==0 then CONST 0
+                         else if n==1 then CONST 1
+                         else if n==2 then TIMES(CONST 2 :: VAR s ::[])
+                         else TIMES ((CONST n)::(POWER (s,n-1))::[]))
+                     else CONST 0
+    | TIMES l ->
+            let rec diffTim ll var =
+                match ll with
+                | [] -> CONST 0
+                | hd::[] -> diff(hd,var)
+                | CONST 0::tl -> CONST 0
+                | CONST x::tl -> TIMES(CONST x::(diffTim tl var)::[])
+                | hd::tl -> SUM(TIMES ((diff(hd,var))::tl)::TIMES (hd::(diffTim tl var)::[])::[])
+            in
+            
+           (match l with 
+                  | [] -> raise InvalidArgument
+                  | hd::[] -> diff(hd,var)
+                  | _ -> (diffTim l var)) 
+    | SUM l -> 
+            let rec diffSum ll var =
+                match ll with
+                | [] -> []
+                | (CONST _)::tl -> diffSum tl var
+                | hd::tl -> (diff(hd,var))::((diffSum tl var))
+            in
+
+            (match l with
+                | [] -> raise InvalidArgument
+                | hd::[] -> diff(hd,var)
+                | _  -> SUM (diffSum l var))
